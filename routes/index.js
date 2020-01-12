b50f6c0aa045f331cd391007efeef744b617293d
Zach Weinberg <zjwgibsg@gmail.com>
2020-01-12 18:24:44 -0500

    Test

--- routes/index.js
+++ routes/index.js
@@ -28,51 +28,56 @@ module.exports = function(app) {
     // Use morgan for HTTP request logging
     app.use(morgan('combined'));
 
-    // Home Page with Click to Call
-    app.get('/', function(request, response) {
-        response.render('index');
-    });
+    app.get('/', (req, res) => { 
+        console.log('testing');
+        res.send('TEST')
+    })
 
-    // Handle an AJAX POST request to place an outbound call
-    app.post('/call', function(request, response) {
-        // This should be the publicly accessible URL for your application
-        // Here, we just use the host for the application making the request,
-        // but you can hard code it or use something different if need be
-        var salesNumber = request.body.salesNumber;
-        var url = 'http://' + request.headers.host + '/outbound/' + encodeURIComponent(salesNumber);
+    // // Home Page with Click to Call
+    // app.get('/', function(request, response) {
+    //     response.render('index');
+    // });
 
-        var options = {
-            to: request.body.phoneNumber,
-            from: config.twilioNumber,
-            url: url,
-        };
+    // // Handle an AJAX POST request to place an outbound call
+    // app.post('/call', function(request, response) {
+    //     // This should be the publicly accessible URL for your application
+    //     // Here, we just use the host for the application making the request,
+    //     // but you can hard code it or use something different if need be
+    //     var salesNumber = request.body.salesNumber;
+    //     var url = 'http://' + request.headers.host + '/outbound/' + encodeURIComponent(salesNumber);
 
-        // Place an outbound call to the user, using the TwiML instructions
-        // from the /outbound route
-        client.calls.create(options)
-          .then((message) => {
-            console.log(message.responseText);
-            response.send({
-                message: 'Thank you! We will be calling you shortly.',
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            response.status(500).send(error);
-          });
-    });
+    //     var options = {
+    //         to: request.body.phoneNumber,
+    //         from: config.twilioNumber,
+    //         url: url,
+    //     };
 
-    // Return TwiML instructions for the outbound call
-    app.post('/outbound/:salesNumber', function(request, response) {
-        var salesNumber = request.params.salesNumber;
-        var twimlResponse = new VoiceResponse();
+    //     // Place an outbound call to the user, using the TwiML instructions
+    //     // from the /outbound route
+    //     client.calls.create(options)
+    //       .then((message) => {
+    //         console.log(message.responseText);
+    //         response.send({
+    //             message: 'Thank you! We will be calling you shortly.',
+    //         });
+    //       })
+    //       .catch((error) => {
+    //         console.log(error);
+    //         response.status(500).send(error);
+    //       });
+    // });
 
-        twimlResponse.say('Thanks for contacting our sales department. Our ' +
-                          'next available representative will take your call. ',
-                          { voice: 'alice' });
+    // // Return TwiML instructions for the outbound call
+    // app.post('/outbound/:salesNumber', function(request, response) {
+    //     var salesNumber = request.params.salesNumber;
+    //     var twimlResponse = new VoiceResponse();
 
-        twimlResponse.dial(salesNumber);
+    //     twimlResponse.say('Thanks for contacting our sales department. Our ' +
+    //                       'next available representative will take your call. ',
+    //                       { voice: 'alice' });
 
-        response.send(twimlResponse.toString());
-    });
+    //     twimlResponse.dial(salesNumber);
+
+    //     response.send(twimlResponse.toString());
+    // });
 };
